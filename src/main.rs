752e84796e448f78c6fbeed3953d3b5410a9d3d8
James Hoffman <purpledungeoncrab@protonmail.com>
2025-07-07 15:44:21 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,285 @@
+use std::{
+    env,
+    path::Path,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
+use gumdrop::Options;
+use log::info;
+use opencv::{
+    core::{Mat, Vector, VectorToVec},
+    imgcodecs,
+};
+use rocket::{
+    fs::{FileServer, NamedFile},
+    get,
+    http::ContentType,
+    post,
+    response::{stream::ByteStream, Redirect},
+    tokio::time::sleep,
+    Request, State,
+};
+use svled::{load_validate_conf, Config, GetEventsFrameBuffer, ManagerData, UnityOptions};
+
+#[macro_use]
+extern crate rocket;
+
+pub mod opensauce;
+
+#[derive(Debug, Options)]
+struct MyOptions {
+    #[options(help = "print help message")]
+    help: bool,
+    #[options(help = "start webserver")]
+    serve: bool,
+    #[options(help = "start the scramble demo")]
+    scramble: bool,
+}
+
+#[derive(Clone)]
+pub struct SvledManagerHolder {
+    pub manager: Arc<Mutex<ManagerData>>,
+    pub unity_options: UnityOptions,
+    pub config_holder_svled: Config,
+    pub frame_buffer: Option<Arc<Mutex<GetEventsFrameBuffer>>>,
+    pub frame_source: Option<Arc<Mutex<bool>>>,
+}
+
+#[get("/")]
+async fn index() -> Result<NamedFile, std::io::Error> {
+    NamedFile::open("static/index.html").await
+}
+
+#[get("/scramble-demo", format = "text/html")]
+async fn scramble_demo_path() -> Result<NamedFile, std::io::Error> {
+    NamedFile::open("static/scramble.html").await
+}
+
+#[post("/start-scramble")]
+async fn scramble_demo_rocket(manager: &State<Arc<Mutex<SvledManagerHolder>>>) -> &'static str {
+    let manager_clone = Arc::clone(manager.inner());
+
+    info!("Starting scramble thread");
+    std::thread::spawn(move || {
+        let (crop_override_tuple, unity_options, config_holder, manager_ref) = {
+            let mgr = manager_clone.lock().unwrap();
+
+            let crop_override_tuple = mgr
+                .config_holder_svled
+                .advanced
+                .crop_override
+                .as_ref()
+                .map(|v| ((v[0], v[1], v[2], v[3]), (v[4], v[5], v[6], v[7])));
+
+            (
+                crop_override_tuple,
+                mgr.unity_options.clone(),
+                mgr.config_holder_svled.clone(),
+                Arc::clone(&mgr.manager),
+            )
+        };
+
+        info!("Starting scramble_demo");
+
+        opensauce::scramble_demo(
+            &crop_override_tuple,
+            &manager_ref,
+            &unity_options,
+            &config_holder,
+            Some(&manager_clone),
+        );
+    });
+
+    "Scramble started"
+}
+
+#[get("/video-cam-1")]
+async fn video_stream_cam_1(
+    manager: &State<Arc<Mutex<SvledManagerHolder>>>,
+) -> (ContentType, ByteStream![Vec<u8>]) {
+    let manager = Arc::clone(manager.inner());
+
+    let stream = ByteStream! {
+        loop {
+            let frame = { // TODO: This is bad
+                let mgr_lock = match manager.lock() {
+                    Ok(lock) => lock,
+                    Err(_) => break,
+                };
+                if mgr_lock.frame_source.is_some() {
+                    if *mgr_lock.frame_source.as_ref().unwrap().lock().unwrap() {
+                        mgr_lock.manager.lock().unwrap().vision.frame_cam_1.clone()
+                    } else {
+                        mgr_lock.frame_buffer.as_ref().unwrap().lock().unwrap().shared_frame_1.clone()
+                    }
+                } else {
+                    mgr_lock.manager.lock().unwrap().vision.frame_cam_1.clone()
+                }
+            };
+
+            let mut buf = Vector::new();
+            if imgcodecs::imencode(".jpg", &frame, &mut buf, &Vector::new()).is_err() {
+                continue;
+            }
+            let jpeg = buf.to_vec();
+
+            yield format!(
+                "--frame\r\nContent-Type: image/jpeg\r\nContent-Length: {}\r\n\r\n",
+                jpeg.len()
+            ).into_bytes();
+            yield jpeg;
+            yield b"\r\n".to_vec();
+
+            sleep(Duration::from_millis(33)).await;
+        }
+    };
+
+    (
+        ContentType::new("multipart", "x-mixed-replace; boundary=frame"),
+        stream,
+    )
+}
+
+#[get("/video-cam-2")]
+async fn video_stream_cam_2(
+    // TODO functionize
+    manager: &State<Arc<Mutex<SvledManagerHolder>>>,
+) -> (ContentType, ByteStream![Vec<u8>]) {
+    let manager = Arc::clone(manager.inner());
+
+    let stream = ByteStream! {
+        loop {
+            let frame = {
+                let mgr_lock = match manager.lock() {
+                    Ok(lock) => lock,
+                    Err(_) => break,
+                };
+                if mgr_lock.frame_source.is_some() {
+                    if *mgr_lock.frame_source.as_ref().unwrap().lock().unwrap() {
+                        mgr_lock.manager.lock().unwrap().vision.frame_cam_2.clone()
+                    } else {
+                        mgr_lock.frame_buffer.as_ref().unwrap().lock().unwrap().shared_frame_2.clone()
+                    }
+                } else {
+                    mgr_lock.manager.lock().unwrap().vision.frame_cam_2.clone()
+                }
+            };
+
+            let mut buf = Vector::new();
+            if imgcodecs::imencode(".jpg", &frame, &mut buf, &Vector::new()).is_err() {
+                continue;
+            }
+            let jpeg = buf.to_vec();
+
+            yield format!(
+                "--frame\r\nContent-Type: image/jpeg\r\nContent-Length: {}\r\n\r\n",
+                jpeg.len()
+            ).into_bytes();
+            yield jpeg;
+            yield b"\r\n".to_vec();
+
+            sleep(Duration::from_millis(33)).await;
+        }
+    };
+
+    (
+        ContentType::new("multipart", "x-mixed-replace; boundary=frame"),
+        stream,
+    )
+}
+
+#[get("/error", format = "text/html")]
+async fn error() -> Result<NamedFile, std::io::Error> {
+    NamedFile::open("static/error.html").await
+}
+
+#[catch(500)]
+fn internal_error(_req: &Request) -> Redirect {
+    Redirect::to(uri!("/error"))
+}
+
+#[rocket::main]
+async fn main() -> Result<(), Box<rocket::Error>> {
+    let opts = MyOptions::parse_args_default_or_exit();
+
+    if opts.help {
+        println!("Usage: --serve or --scramble");
+        return Ok(());
+    }
+
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    let config_load_result = load_validate_conf(Path::new("svled.toml"));
+
+    let (manager, unity_options, config_holder) = (
+        Arc::new(Mutex::new(config_load_result.0)),
+        config_load_result.1,
+        config_load_result.2,
+    );
+
+    let svled_manager = Arc::new(Mutex::new(SvledManagerHolder {
+        manager,
+        unity_options,
+        config_holder_svled: config_holder,
+        frame_buffer: None,
+        frame_source: None,
+    }));
+
+    if opts.scramble {
+        scramble_demo_rocket(State::from(&svled_manager.clone())).await;
+        return Ok(());
+    }
+
+    if opts.serve {
+        info!("Starting webserver!");
+
+        info!("Initializing svled...");
+        let config_load_result = load_validate_conf(Path::new("svled.toml"));
+
+        let (manager_svled, unity_options, config_holder_svled) = (
+            Arc::new(Mutex::new(config_load_result.0)),
+            config_load_result.1,
+            config_load_result.2,
+        );
+
+        // Set default image for /video
+        manager_svled.lock().unwrap().vision.frame_cam_1 =
+            imgcodecs::imread("static/images/hatred.jpg", imgcodecs::IMREAD_COLOR).unwrap();
+
+        manager_svled.lock().unwrap().vision.frame_cam_2 =
+            imgcodecs::imread("static/images/hatred.jpg", imgcodecs::IMREAD_COLOR).unwrap();
+
+        let svled_manager = Arc::new(Mutex::new(SvledManagerHolder {
+            manager: manager_svled,
+            unity_options,
+            config_holder_svled,
+            frame_buffer: Some(Arc::new(Mutex::new(GetEventsFrameBuffer {
+                shared_frame_1: Mat::default(),
+                shared_frame_2: Mat::default(),
+            }))),
+            frame_source: Some(Arc::new(Mutex::new(true))),
+        }));
+
+        let rocket = rocket::build()
+            .mount(
+                "/",
+                routes![
+                    index,
+                    error,
+                    video_stream_cam_1,
+                    video_stream_cam_2,
+                    scramble_demo_path,
+                    scramble_demo_rocket,
+                ],
+            )
+            .mount("/static", FileServer::from("./static"))
+            .register("/", catchers![internal_error])
+            .manage(svled_manager);
+
+        rocket.launch().await?;
+    }
+
+    Ok(())
+}
---
+++ src/opensauce.rs
@@ -0,0 +1,126 @@
+// This will contain any Open Sauce specific code, that way the rest can stay clean.
+use std::{
+    fs::remove_file,
+    path::Path,
+    sync::{Arc, Mutex},
+    thread::{self, sleep},
+    time::Duration,
+};
+
+use log::{debug, error, info};
+use svled::{scan, signal_restart, unity, Config, ManagerData, UnityOptions};
+
+use crate::SvledManagerHolder;
+
+type CropData = Option<((i32, i32, i32, i32), (i32, i32, i32, i32))>;
+
+pub fn scramble_demo(
+    crop_data: &CropData,
+    manager: &Arc<Mutex<ManagerData>>,
+    unity_options: &UnityOptions,
+    config_holder: &Config,
+    svled_manager: Option<&Arc<Mutex<SvledManagerHolder>>>, // Holds frame buffer, and frame_source which is controlled by get_events.
+) {
+    info!("Starting scramble demo");
+    let mut first_run = true;
+
+    let signal_file = Path::new("./end_loop");
+    if signal_file.exists() {
+        remove_file(signal_file).expect("Unable to write signal file end_loop");
+    }
+
+    let cloned_manager = Arc::clone(manager);
+
+    info!("Entering loop");
+
+    loop {
+        if first_run || signal_file.exists() {
+            first_run = false;
+
+            info!("Re-scanning");
+
+            if signal_file.exists() {
+                manager.lock().unwrap().state.keepalive = false;
+                remove_file(signal_file).unwrap();
+                info!("signal file exists, signaling restart");
+                signal_restart(
+                    config_holder.unity_options.unity_ip,
+                    config_holder.unity_options.unity_ports[0],
+                );
+            }
+
+            if let Some(manager) = svled_manager {
+                if let Some(ref frame_source) = manager.lock().unwrap().frame_source {
+                    *frame_source.lock().unwrap() = true;
+                }
+            }
+
+            match scan(config_holder.clone(), manager, true, *crop_data) {
+                Ok(_) => {}
+                Err(e) => {
+                    error!("could not scan with multiple cameras and streamlined enabled: {e}")
+                }
+            };
+
+            info!("Sending current positions to Unity");
+
+            match unity::send_pos(unity_options.clone()) {
+                Ok(_) => {}
+                Err(e) => {
+                    panic!("There was an issue connecting to Unity: {e}")
+                }
+            };
+
+            info!("Starting listener thread");
+            if let Some(manager) = svled_manager {
+                if let Some(ref frame_source) = manager.lock().unwrap().frame_source {
+                    *frame_source.lock().unwrap() = false;
+                }
+            }
+            let owned_options = unity_options.clone();
+            let owned_manager = Arc::clone(&cloned_manager);
+            let owned_config = config_holder.clone();
+
+            while !manager.lock().unwrap().state.keepalive {
+                // get_events will reset this once it has properly exited
+                sleep(Duration::new(0, 500000));
+            }
+
+            manager.lock().unwrap().state.keepalive = true;
+
+            let owned_frame_buffer = svled_manager.as_ref().and_then(|manager| {
+                manager
+                    .lock()
+                    .ok()
+                    .and_then(|locked| locked.frame_buffer.as_ref().map(Arc::clone))
+            });
+
+            thread::spawn(move || {
+                debug!("inside thread");
+                match unity::get_events(
+                    owned_manager,
+                    &owned_options,
+                    &owned_config,
+                    &owned_options.unity_ports.clone()[0],
+                    &owned_frame_buffer,
+                ) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        panic!("get_events thread crashed with error: {e}")
+                    }
+                }
+            });
+        }
+    }
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+pub fn webserver() -> _ {
+    info!("Starting rocket...");
+    rocket::build().mount("/", routes![index])
+}
